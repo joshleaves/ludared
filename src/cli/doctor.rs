257ac3e8dcdab880afab7b3d3250c23ba87b7295
red <red@dreamleaves.org>
2026-04-30 20:23:23 +0200

    feature(cli/doctor): Move source verification from Project to doctor command

--- src/cli/doctor.rs
+++ src/cli/doctor.rs
@@ -1,15 +1,58 @@
-use crate::{app_error::AppError, project::Project};
 use log::*;
+use sha2::{Digest, Sha256};
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::path::PathBuf;
+
+use crate::{app_error::AppError, manifest::Source, project::Project};
 
 pub(crate) fn command_doctor() -> Result<(), AppError> {
   let project = Project::load_default()?;
-  info!(
-    "Verifying sources for project {}",
-    project.configuration.project.name
-  );
-  project.verify_sources()?;
+  info!("Verifying sources for project {}", project.name());
+  if project.manifest.sources.is_empty() {
+    warn!("No source files defined in project");
+    return Ok(());
+  }
+  let sources_path = project.root.join(project.configuration.paths.sources);
+  for (source_name, source) in project.manifest.sources.iter() {
+    debug!("Verifying source file: {}", source_name.display());
+    verify_source(sources_path.join(source_name), source)?;
+  }
+
+  // project.verify_sources()?;
   println!("✓ All sources OK");
 
   println!("✓ Doctor check passed");
+  Ok(())
+}
+
+fn verify_source(source_path: PathBuf, source: &Source) -> Result<(), AppError> {
+  let file =
+    File::open(&source_path).map_err(|e| AppError::SourceFileIo(source_path.clone(), e))?;
+
+  let mut reader = BufReader::new(file);
+  let mut hasher = Sha256::new();
+  let mut buffer = [0u8; 8192];
+
+  loop {
+    let n = reader
+      .read(&mut buffer)
+      .map_err(|e| AppError::SourceFileIo(source_path.clone(), e))?;
+    if n == 0 {
+      break;
+    }
+    hasher.update(&buffer[..n]);
+  }
+  let actual = hex::encode(hasher.finalize());
+
+  if actual != source.sha256 {
+    return Err(AppError::SourceFileMismatch(
+      source_path.clone(),
+      source.sha256.clone(),
+      actual,
+    ));
+  }
+  debug!("Matching SHA256: {}", actual);
+
   Ok(())
 }
--- src/project.rs
+++ src/project.rs
@@ -1,12 +1,9 @@
 use log::*;
-use sha2::{Digest, Sha256};
-use std::fs::File;
-use std::io::{BufReader, Read};
 use std::path::{Path, PathBuf};
 
 use crate::app_error::AppError;
 use crate::configuration::Configuration;
-use crate::manifest::{Manifest, Source};
+use crate::manifest::Manifest;
 
 #[derive(Debug)]
 pub(crate) struct Project {
@@ -17,11 +14,13 @@ pub(crate) struct Project {
 
 impl Project {
   pub(crate) fn load(root: &Path) -> Result<Self, AppError> {
+    trace!("Loading up project configuration");
     let configuration_path = root.join("ludared.toml");
     let configuration = Configuration::load(&configuration_path)?;
     debug!("Parsed configuration: ludared.toml");
     trace!("{configuration:?}");
 
+    trace!("Loading up project manifest");
     let manifest_path = root.join(&configuration.project.manifest);
     let manifest = Manifest::load(&manifest_path)?;
     debug!(
@@ -41,56 +40,7 @@ impl Project {
     Self::load(Path::new("."))
   }
 
-  pub(crate) fn sources_path(&self) -> PathBuf {
-    self.root.join(&self.configuration.paths.sources)
-  }
-
-  pub(crate) fn source_path(&self, source_name: &Path) -> PathBuf {
-    self.sources_path().join(source_name)
-  }
-
-  pub(crate) fn verify_sources(&self) -> Result<(), AppError> {
-    if self.manifest.sources.is_empty() {
-      warn!("No source files defined in project");
-      return Ok(());
-    }
-
-    for (source_name, source) in self.manifest.sources.iter() {
-      debug!("Verifying source file: {}", source_name.display());
-      self.verify_source(source_name, source)?;
-    }
-    Ok(())
-  }
-
-  pub(crate) fn verify_source(&self, source_name: &Path, source: &Source) -> Result<(), AppError> {
-    let source_path = self.source_path(source_name);
-    let file =
-      File::open(&source_path).map_err(|e| AppError::SourceFileIo(source_path.clone(), e))?;
-
-    let mut reader = BufReader::new(file);
-    let mut hasher = Sha256::new();
-    let mut buffer = [0u8; 8192];
-
-    loop {
-      let n = reader
-        .read(&mut buffer)
-        .map_err(|e| AppError::SourceFileIo(source_path.clone(), e))?;
-      if n == 0 {
-        break;
-      }
-      hasher.update(&buffer[..n]);
-    }
-    let actual = hex::encode(hasher.finalize());
-
-    if actual != source.sha256 {
-      return Err(AppError::SourceFileMismatch(
-        source_path.clone(),
-        source.sha256.clone(),
-        actual,
-      ));
-    }
-    debug!("Matching SHA256: {}", actual);
-
-    Ok(())
+  pub fn name(&self) -> &String {
+    &self.configuration.project.name
   }
 }
